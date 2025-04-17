af9ff8c9e7873b1c5d0206e619100e640bca2eca
Lemi Dinku <lemidinq@gmail.com>
2025-04-17 17:44:01 +0300

    feat: add nitro plugin to fix response rendering issue

---
+++ server/plugins/veutify.fix.ts
@@ -0,0 +1,6 @@
+export default defineNitroPlugin((nitroApp: any) => {
+    nitroApp.hooks.hook('render:response', (response: any) => {
+      response.body = response.body.replaceAll('/_nuxt/\0', '/_nuxt/')
+    })
+  })
+  
